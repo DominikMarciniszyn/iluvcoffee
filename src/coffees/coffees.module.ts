9cc6d5ef21f4da5d27b13493f8850ef18630404b
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2022-04-03 11:43:35 +0200

    feat: added coffee rating module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,10 +4,12 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { Event } from '../events/entities/event.entity';
 
 @Module({
   controllers: [CoffeesController],
   providers: [CoffeesService],
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
