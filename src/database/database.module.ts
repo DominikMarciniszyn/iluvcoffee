cc523d4d7e9b15be479da36c8895622a3aa8b9d3
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2022-04-09 12:45:55 +0200

    feat: created dynamic database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConnectionOptions, createConnection } from 'typeorm';
+
+@Module({})
+export class DatabaseModule {
+  static register(options: ConnectionOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: 'CONNECTION',
+          useValue: createConnection(options),
+        },
+      ],
+    };
+  }
+}
+
+/*  Utilizing the dynamic DatabaseModule in another Modules imports: []
+imports: [
+  DatabaseModule.register({ // 👈 passing in dynamic values
+    type: 'postgres',
+    host: 'localhost',
+    password: 'password',
+  })
+]
+*/
