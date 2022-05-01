1d56b9b18746de5d56b5bf6589ba83b43c78c456
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2022-05-01 08:39:57 +0200

    feat: added custom decorator and updated guard to use new logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
 import * as Joi from '@hapi/joi';
 
 @Module({
@@ -30,6 +31,7 @@ import * as Joi from '@hapi/joi';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
