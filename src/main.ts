2b9629780c2d98f1d1573a325291507ff1d38f3b
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2022-05-01 08:21:26 +0200

    feat: added api key guard to the project

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filter/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalGuards(new ApiKeyGuard());
   app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
